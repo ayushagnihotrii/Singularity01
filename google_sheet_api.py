81ff39967e23dd28c59b2015245ecdd2d2fe7483
Ayush Agnihotri <omg.uno.reverse@gmail.com>
2026-02-27 01:29:02 +0530

    Refactor write_data_to_sheet for clarity

--- google_sheet_api.py
+++ google_sheet_api.py
@@ -7,19 +7,22 @@ google_creds = json.loads(os.environ["GOOGLE_CREDENTIALS"])
 
 credentials = service_account.Credentials.from_service_account_info(
     google_creds,
-    scopes=["https://www.googleapis.com/auth/spreadsheets"]
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive"
+    ]
 )
 
 client = gspread.authorize(credentials)
 
 SHEET_NAME = "singularity01"
 
- def write_data_to_sheet(ip_address, timestamp):
-sheet = client.open(SHEET_NAME).sheet1
-    
-    # Get current number of rows (excluding header)
-current_rows = len(sheet.get_all_values())
-    
-serial_no = current_rows  # since row 1 is header
 
-sheet.append_row([serial_no, timestamp, ip_address])
+def write_data_to_sheet(ip_address, timestamp):
+    sheet = client.open(SHEET_NAME).sheet1
+
+    # Count rows to generate serial number
+    current_rows = len(sheet.get_all_values())
+    serial_no = current_rows  # header already occupies row 1
+
+    sheet.append_row([serial_no, timestamp, ip_address])
